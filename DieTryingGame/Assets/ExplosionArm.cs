5e8e982b748daca5998d31f1c8a187d090bba52c
ChimiChangle <andychang43@gmail.com>
2025-02-23 00:20:52 -0500

    POLISh

--- DieTryingGame/Assets/ExplosionArm.cs
+++ DieTryingGame/Assets/ExplosionArm.cs
@@ -2,13 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterExplosionBehaviour : StateMachineBehaviour
+public class ExplosionArm : StateMachineBehaviour
 {
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        animator.gameObject.GetComponent<BodyExplosion>().ExplosionHit();
-        animator.gameObject.GetComponent<BodyExplosion>().ExplodeSound.Play();
+        animator.gameObject.GetComponent<BodyExplosion>().ArmSound.Play();
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
@@ -18,10 +17,10 @@ public class CharacterExplosionBehaviour : StateMachineBehaviour
     //}
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        Destroy(animator.gameObject);
-    }
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
